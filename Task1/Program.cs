59c86877c917051fd212812134210d2fcbb00214
Tape <trid.skivi@gmail.com>
2022-07-04 19:24:19 +0300

    wrote the solution to the first task

--- Task1/Program.cs
+++ Task1/Program.cs
@@ -1,2 +1,24 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задача 27: Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+
+// 452 -> 11
+
+// 82 -> 10
+
+// 9012 -> 12
+
+int AddValuesNumber (int arg) {
+    int sumNum = 0;
+    int temp = arg;
+    while (temp != 0) {
+        sumNum = sumNum + temp % 10;
+        temp /= 10;
+    }
+    return sumNum;
+}
+
+Console.WriteLine("Enter the number");
+int num = Convert.ToInt32(Console.ReadLine());
+
+int result = AddValuesNumber(num);
+
+Console.WriteLine("the sum of the values in a number " + result);
